93f3df7badc17d32c428435d8d31e22e77fc0eb5
Maksim <borisovets.maksim@gmail.com>
2021-07-14 10:28:15 +0300

    add project files

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(90)))), ((int)(((byte)(49)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.label2.Name = "label2";
+            // 
+            // radioButton1
+            // 
+            resources.ApplyResources(this.radioButton1, "radioButton1");
+            this.radioButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            resources.ApplyResources(this.radioButton2, "radioButton2");
+            this.radioButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            resources.ApplyResources(this.radioButton3, "radioButton3");
+            this.radioButton3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.TabStop = true;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.label3.Name = "label3";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(40)))), ((int)(((byte)(49)))));
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private const string Path = @"D:\text.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            radioButton1.Checked = true;
+        }
+            private void Button1_Click(object sender, EventArgs e)
+            {
+                if (radioButton1.Checked)
+                {
+                    _ = new Process();
+
+                    Process p = new Process();
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.FileName = "созданиеE.bat";
+                    p.Start();
+
+                    string output = p.StandardOutput.ReadToEnd();
+                    p.WaitForExit();
+                }
+
+                else if (radioButton2.Checked)
+                {
+                    _ = new Process();
+
+                    Process p = new Process();
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.FileName = "созданиеF.bat";
+                    p.Start();
+
+                    string output = p.StandardOutput.ReadToEnd();
+                    p.WaitForExit();
+                }
+
+                else if (radioButton3.Checked)
+                {
+                    _ = new Process();
+
+                    Process p = new Process();
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.FileName = "созданиеG.bat";
+                    p.Start();
+
+                    string output = p.StandardOutput.ReadToEnd();
+                    p.WaitForExit();
+                }
+
+                radioButton1.Enabled = false;
+                radioButton2.Enabled = false;
+                radioButton3.Enabled = false;
+
+                int a = 0,
+                    i,
+                    five = 500,
+                    b = 0,
+                    number;
+
+                using (StreamReader fs = new StreamReader(Path))
+                {
+                    while (true)
+                    {
+                        string temp = fs.ReadLine();
+                        a++;
+                        if (temp == null)
+                            break;
+                    }
+                }
+
+                a--;
+
+                label2.Text = a + " files";
+                label3.Text = "Rename...";
+
+                string newneme = "";
+                int[] arr = new int[five];
+                int[] Arr = new int[five];
+
+                for (i = 0; i < 500; i++)
+                    arr[i] = i + 1;
+                for (i = 0; i < 500; i++)
+                    Arr[i] = i + 501;
+
+                using (StreamReader fs = new StreamReader(Path))
+                {
+                    while (b < a)
+                    {
+                        string temp = fs.ReadLine();
+
+                        Random rnd = new Random();
+                        i = rnd.Next(0, five);
+                        number = Arr[i];
+                        five--;
+                        Arr[i] = Arr[five];
+
+                        newneme = Convert.ToString(number);
+                        if (radioButton1.Checked)
+                        {
+                            try
+                            {
+                                System.IO.File.Move("E:\\" + temp, "E:\\" + newneme + ".mp3");
+                            }
+                            catch (Exception) {}
+                        }
+                        if (radioButton2.Checked)
+                        {
+                            try
+                            {
+                                System.IO.File.Move("F:\\" + temp, "F:\\" + newneme + ".mp3");
+                            }
+                            catch (Exception) { }
+                        }
+                        if (radioButton3.Checked)
+                        {
+                            try
+                            {
+                                System.IO.File.Move("G:\\" + temp, "G:\\" + newneme + ".mp3");
+                            }
+                            catch (Exception) { }
+                        }
+
+                        b++;
+
+                        if (temp == "")
+                        {
+                            _ = new Process();
+
+                            Process p = new Process();
+                            p.StartInfo.UseShellExecute = false;
+                            p.StartInfo.RedirectStandardOutput = true;
+                            p.StartInfo.CreateNoWindow = true;
+                            p.StartInfo.FileName = "удаление.bat";
+                            p.Start();
+
+                            string output = p.StandardOutput.ReadToEnd();
+                            p.WaitForExit();
+                            break;
+                        }
+                    }
+                }
+
+                if (radioButton1.Checked)
+                {
+                    _ = new Process();
+
+                    Process p = new Process();
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.FileName = "созданиеE.bat";
+                    p.Start();
+
+                    string output = p.StandardOutput.ReadToEnd();
+                    p.WaitForExit();
+                }
+
+                if (radioButton2.Checked)
+                {
+                    _ = new Process();
+
+                    Process p = new Process();
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.FileName = "созданиеF.bat";
+                    p.Start();
+
+                    string output = p.StandardOutput.ReadToEnd();
+                    p.WaitForExit();
+                }
+
+                if (radioButton3.Checked)
+                {
+                    _ = new Process();
+
+                    Process p = new Process();
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.FileName = "созданиеG.bat";
+                    p.Start();
+
+                    string output = p.StandardOutput.ReadToEnd();
+                    p.WaitForExit();
+                }
+
+                using (StreamReader fs = new StreamReader(Path))
+                {
+                    b = 0;
+
+                    while (b < a)
+                    {
+                        string temp = fs.ReadLine();
+
+                        Random rnd = new Random();
+                        i = rnd.Next(0, five);
+                        number = arr[i];
+                        five--;
+                        arr[i] = arr[five];
+
+                        newneme = Convert.ToString(number);
+                        if (radioButton1.Checked)
+                        {
+                            try
+                            {
+                                System.IO.File.Move("E:\\" + temp, "E:\\" + newneme + ".mp3");
+                            }
+                            catch (Exception) { }
+                        }
+                        if (radioButton2.Checked)
+                        {
+                            try
+                            {
+                                System.IO.File.Move("F:\\" + temp, "F:\\" + newneme + ".mp3");
+                            }
+                            catch (Exception) { }
+                        }
+                        if (radioButton3.Checked)
+                        {
+                            try
+                            {
+                                System.IO.File.Move("G:\\" + temp, "G:\\" + newneme + ".mp3");
+                            }
+                            catch (Exception) { }
+                        }
+
+                        b++;
+
+                        if (temp == "")
+                        {
+                            _ = new Process();
+
+                            Process p = new Process();
+                            p.StartInfo.UseShellExecute = false;
+                            p.StartInfo.RedirectStandardOutput = true;
+                            p.StartInfo.CreateNoWindow = true;
+                            p.StartInfo.FileName = "удаление.bat";
+                            p.Start();
+
+                            string output = p.StandardOutput.ReadToEnd();
+                            p.WaitForExit();
+                            break;
+                        }
+                    }
+                }
+
+                label3.Text = "Remove...";
+
+                if (radioButton1.Checked)
+                {
+                    _ = new Process();
+
+                    Process p = new Process();
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.FileName = "перемещениеE.bat";
+                    p.Start();
+
+                    string output = p.StandardOutput.ReadToEnd();
+                    p.WaitForExit();
+                }
+
+                if (radioButton2.Checked)
+                {
+                    _ = new Process();
+
+                    Process p = new Process();
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.FileName = "перемещениеF.bat";
+                    p.Start();
+
+                    string output = p.StandardOutput.ReadToEnd();
+                    p.WaitForExit();
+                }
+
+                if (radioButton3.Checked)
+                {
+                    _ = new Process();
+
+                    Process p = new Process();
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.FileName = "перемещениеG.bat";
+                    p.Start();
+
+                    string output = p.StandardOutput.ReadToEnd();
+                    p.WaitForExit();
+                }
+
+                if (true)
+                {
+                    _ = new Process();
+
+                    Process p = new Process();
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.FileName = "удаление.bat";
+                    p.Start();
+
+                    string output = p.StandardOutput.ReadToEnd();
+                    p.WaitForExit();
+                }
+
+                label3.Text = "FINISH";
+            }
+            private void Form1_Load(object sender, EventArgs e)
+            {
+
+            }
+        
+    }
+}
